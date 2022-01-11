7bf240c02e1447d7e5b690d610a08beb9c5a49dd
KarloPoslek <karlo.poslek@gmail.com>
2022-01-11 21:43:01 +0100

    Finished exercises 1 and 2.

--- ES6/1_letScope.js
+++ ES6/1_letScope.js
@@ -1,10 +1,11 @@
-var printNumTwo;
-for (var i = 0; i < 3; i++) {
-  if (i === 2) {
-    printNumTwo = function() {
-      return i;
-    };
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
   }
-}
-console.log(printNumTwo());
-console.log(printNumTwo());
+  
+  checkScope();
