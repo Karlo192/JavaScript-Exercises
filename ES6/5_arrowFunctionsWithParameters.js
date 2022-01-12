ca9b29aef400a8383498448518909d82b936a93e
KarloPoslek <karlo.poslek@gmail.com>
2022-01-12 14:20:19 +0100

    Finished exercise 6.

--- ES6/5_arrowFunctionsWithParameters.js
+++ ES6/5_arrowFunctionsWithParameters.js
@@ -1,3 +1,11 @@
 const doubler = (item) => item * 2;
 console.log(doubler(4));
 
+const multiplier = (item, multi) => item * multi;
+console.log(multiplier(4, 2));
+
+const myConcat =(arr1, arr2) => {
+    return arr1.concat(arr2);
+  };
+  
+  console.log(myConcat([1, 2], [3, 4, 5]));
