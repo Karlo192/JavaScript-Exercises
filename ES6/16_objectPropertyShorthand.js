126eeaa1226e90b76924748a929e46d4d0038867
KarloPoslek <karlo.poslek@gmail.com>
2022-01-16 17:45:14 +0100

    Changes made.

---
+++ ES6/16_objectPropertyShorthand.js
@@ -0,0 +1,21 @@
+const getMousePosition = (x, y) => ({
+    x: x,
+    y: y
+});
+
+//syntactic sugar
+const getMousePosition = (x, y) => ({ x, y });
+
+
+
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name: name,
+      age: age,
+      gender: gender
+    };
+    // Only change code above this line
+};
+
+
