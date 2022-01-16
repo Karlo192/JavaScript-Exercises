ef626d232440163102b3705f0a330b4d6f3bd28b
KarloPoslek <karlo.poslek@gmail.com>
2022-01-16 18:37:19 +0100

    Finished exercise 17.

--- ES6/17_conciseDeclerativeFunctions.js
+++ ES6/17_conciseDeclerativeFunctions.js
@@ -15,3 +15,27 @@ const person = {
     return `Hello! My name is ${this.name}.`;
   }
 };
+
+
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear: function(newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+      this.gear = newGear;
+    }
+  };
+  // Only change code above this line
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
