0a0df402899d3639ba00ba5b593d281f422e0fa5
KarloPoslek <karlo.poslek@gmail.com>
2022-01-27 22:08:07 +0100

    Changes made

--- ES6/13_destructuringAssignment5.js
+++ ES6/13_destructuringAssignment5.js
@@ -11,6 +11,7 @@
 // Variables a and b take the first and second values from the array. After that, because of the rest parameter's presence, arr gets the rest of the values in the form of an array. The rest element only works correctly as the last variable in the list. As in, you cannot use the rest parameter to catch a subarray that leaves out the last element of the original array.
 
 
+
 // EXERCISE:
 // Use destructuring assignment with the rest parameter to perform an effective Array.prototype.slice() so that arr is a sub-array of the original array source with the first two elements omitted. 
 
