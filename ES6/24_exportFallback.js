39e0e95efa87df0a52e7bc382a3d8f1f28c89cc7
KarloPoslek <karlo.poslek@gmail.com>
2022-01-30 21:38:50 +0100

    Finished exercise 24.

--- ES6/24_exportFallback.js
+++ ES6/24_exportFallback.js
@@ -20,7 +20,6 @@ export default function(x, y) {
 // EXERCISE:
 // The following function should be the fallback value for the module. Please add the necessary code to do so.
 
-function subtract(x, y) {
+export default function subtract(x, y) {
     return x - y;
   }
-  
