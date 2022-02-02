cf90c29756fbd65972c5444cfe24f3863276d8f1
KarloPoslek <karlo.poslek@gmail.com>
2022-02-02 21:21:27 +0100

    Finished exercise 11.

--- 2_RegularExpressions/11_matchSingleCharactersNotSpecified.js
+++ 2_RegularExpressions/11_matchSingleCharactersNotSpecified.js
@@ -8,3 +8,7 @@
 
 // EXERCISE
 // Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou^0-9]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
