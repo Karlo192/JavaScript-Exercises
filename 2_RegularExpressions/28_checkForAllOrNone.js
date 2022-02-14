2c7da6571683cd938ccd8c2234e6ab6a72f0bce9
KarloPoslek <karlo.poslek@gmail.com>
2022-02-14 16:38:59 +0100

    Finished exercise 28.

--- 2_RegularExpressions/28_checkForAllOrNone.js
+++ 2_RegularExpressions/28_checkForAllOrNone.js
@@ -17,3 +17,6 @@ rainbowRegex.test(british);
 // EXERCISE:
 // Change the regex favRegex to match both the American English (favorite) and the British English (favourite) version of the word.
 
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
