2151f18c4f6909573d0636fcfaf970df02f092ec
KarloPoslek <karlo.poslek@gmail.com>
2022-02-14 16:43:03 +0100

    Finished exercise 29.

--- 2_RegularExpressions/29_positiveAndNegativeLookahead.js
+++ 2_RegularExpressions/29_positiveAndNegativeLookahead.js
@@ -28,3 +28,6 @@ checkPass.test(password);
 // EXERCISE:
 // Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long, and have two consecutive digits.
 
+let sampleWord = "astronaut";
+let pwRegex =  /(?=\w{6})(?=\w*\d{2})/;
+let result = pwRegex.test(sampleWord);
