0d23fb36979b3b230094e184222e1019cd564ab9
KarloPoslek <karlo.poslek@gmail.com>
2022-02-14 17:11:01 +0100

    Finished exercise 33.

--- 2_RegularExpressions/33_removeWhitespaceFromStartAndEnd.js
+++ 2_RegularExpressions/33_removeWhitespaceFromStartAndEnd.js
@@ -7,4 +7,6 @@
 
 // Note: The String.prototype.trim() method would work here, but you'll need to complete this challenge using regular expressions.
 
-
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let result = hello.replace(wsRegex, ""); // Change this line
