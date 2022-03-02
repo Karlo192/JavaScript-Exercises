9e8f7f265d7cc524ab4b7e0a001bf2ab8d8cde20
KarloPoslek <karlo.poslek@gmail.com>
2022-03-02 21:57:47 +0100

    Finished exercise 1.

--- 4_BasicDataStructures/1_storeACollectionOfData.js
+++ 4_BasicDataStructures/1_storeACollectionOfData.js
@@ -36,3 +36,5 @@ let complexArray = [
 
 // EXERCISE:
 // We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+
+let yourArray = [1, 2, 3, 'hey', true]; // Change this line
