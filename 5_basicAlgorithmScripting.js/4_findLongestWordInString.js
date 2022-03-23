7fb1c8777b66f260b5bedaf323ee39760f1d97b9
KarloPoslek <karlo.poslek@gmail.com>
2022-03-23 10:28:35 +0100

    Finished exercise 4.

---
+++ 5_basicAlgorithmScripting.js/4_findLongestWordInString.js
@@ -0,0 +1,20 @@
+
+// Find the Longest Word in a String
+
+// Return the length of the longest word in the provided sentence.
+
+// Your response should be a number.
+
+
+function findLongestWordLength(str) {
+    let words = str.split(' ');
+    let maxLength = 0;
+  
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > maxLength) {
+        maxLength = words[i].length;
+      }
+    }
+  
+    return maxLength;
+  }
