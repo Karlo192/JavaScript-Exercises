57ba773b0efe3c5c80c8c076b4fba83b1bc5a2f1
KarloPoslek <karlo.poslek@gmail.com>
2022-03-26 23:30:56 +0100

    Changes

--- 5_basicAlgorithmScripting.js/6_confirmTheEnding.js
+++ 5_basicAlgorithmScripting.js/6_confirmTheEnding.js
@@ -5,4 +5,3 @@
 
 // This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
 
-
