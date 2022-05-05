061f594e9fbf5a5bf7bb3fd90adda9c3c21b5e7e
KarloPoslek <karlo.poslek@gmail.com>
2022-05-05 21:31:00 +0200

    Finished exercise 10.

--- 6_objectOrientedProgramming/10_reduceDuplicateCode.js
+++ 6_objectOrientedProgramming/10_reduceDuplicateCode.js
@@ -20,3 +20,11 @@ console.log(canary.numLegs);
 // EXERCISE:
 // Add a numLegs property to the prototype of Dog
 
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  // Only change code above this line
+  let beagle = new Dog("Snoopy");
